2f46763f40faf5df5719136903b95e2c92d9a0ba
bigbadboy1010 <57946124+bigbadboy1010@users.noreply.github.com>
2026-08-06 08:08:03 +0200

    remove deprecated C string conversion

--- Sources/AgenTM5N/Services/HardwareService.swift
+++ Sources/AgenTM5N/Services/HardwareService.swift
@@ -23,6 +23,11 @@ public enum HardwareService {
     guard sysctlbyname(name, &buffer, &size, nil, 0) == 0 else {
       return nil
     }
-    return String(cString: buffer)
+
+    let bytes = buffer
+      .prefix { $0 != 0 }
+      .map { UInt8(bitPattern: $0) }
+
+    return String(decoding: bytes, as: UTF8.self)
   }
 }
